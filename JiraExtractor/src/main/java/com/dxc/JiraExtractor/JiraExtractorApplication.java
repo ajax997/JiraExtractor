9aab3a0fe0ffaacdc403ed44bfe245eda7ef2899
nguyennghi <nguyennghi1997@outlook.con>
2019-04-01 10:50:08 +0700

    skeleton updated

--- JiraExtractor/src/main/java/com/dxc/JiraExtractor/JiraExtractorApplication.java
+++ JiraExtractor/src/main/java/com/dxc/JiraExtractor/JiraExtractorApplication.java
@@ -2,10 +2,14 @@ package com.dxc.JiraExtractor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class JiraExtractorApplication {
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JiraExtractorApplication.class, args);
 	}
--- JiraExtractor/src/main/java/com/dxc/JiraExtractor/controllers/WelcomeController.java
+++ JiraExtractor/src/main/java/com/dxc/JiraExtractor/controllers/WelcomeController.java
@@ -1,13 +1,31 @@
 package com.dxc.JiraExtractor.controllers;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WelcomeController {
-	@GetMapping("/")
-	public String indexPage()
-	{
-		return "<h1>Hello World!</h1>";
+	
+	// inject via application.properties
+		
+
+		@RequestMapping("/")
+		public String welcome() {
+			
+			return "welcome";
+		}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(@RequestParam("username") String user, @RequestParam("password") String password) {
+		return "True";
+		
 	}
 }
